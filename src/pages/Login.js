3839d1eb2bf8fab7375782be51e839ee5f29bc5c
Caio Cesar <105387511+caiocesardev@users.noreply.github.com>
2024-09-27 00:39:40 -0300

    feat: Estilização da pagina de Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import {
   Button,
@@ -7,66 +8,79 @@ import {
   Box,
   Grid2,
 } from "@mui/material";
+import '../styles/Login.scss';  
 
 const Login = () => {
   const enviarFormulario = (event) => {
     event.preventDefault();
     const dados = new FormData(event.currentTarget);
     const dadosLogin = {
-      email: dados.get('email'),
-      senha: dados.get('password'),
+      email: dados.get("email"),
+      senha: dados.get("password"),
     };
     console.log(dadosLogin);
   };
+
   return (
-    <Container component="main" maxWidth="xs">
-      <Box
-        sx={{
-          marginTop: 8,
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-        }}
-      >
-        <Typography component="h1" variant="h5">
-          Login
-        </Typography>
-        <Box component="form" onSubmit={enviarFormulario} noValidate sx={{ mt: 1}}>
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            label="Email"
-            name="email"
-            autoComplete="email"
-            autoFocus
-          />
-          <TextField 
-          margin="normal"
-          required
-          fullWidth
-          name="password"
-          label="Senha"
-          type="password"
-          id="password"
-          />
-          <Button 
-          type="submit"
-          fullWidth
-          variant="contained"
-          sx= {{mt: 3, mb: 2}}
+    <div className="login-background"> 
+      <Container component="main" maxWidth="xs">
+        <Box className="login-container">
+          <Typography component="h1" variant="h3" className="login-title">
+            Academi<span style={{ color: "#1976d2" }}>Track</span>
+          </Typography>
+          <Box
+            component="form"
+            onSubmit={enviarFormulario}
+            noValidate
+            sx={{ mt: 1 }}
           >
-            Entrar
-          </Button>
-          <Grid2 container>
-            <Grid2 item>
-            <a href="#">Não lembra a senha? Recuperara senha</a>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email"
+              name="email"
+              autoComplete="email"
+              autoFocus
+              aria-label="Campo de email"
+            />
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Senha"
+              type="password"
+              id="password"
+              aria-label="Campo de senha"
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              className="login-button" 
+            >
+              Entrar
+            </Button>
+            <Grid2 container>
+              <Grid2 item>
+                <a
+                  href="#"
+                  style={{
+                    textDecoration: 'none',
+                    color: '#1976d2',
+                  }}
+                  aria-label="Link para recuperar senha"
+                >
+                  Esqueci minha senha.
+                </a>
+              </Grid2>
             </Grid2>
-          </Grid2>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
+    </div>
   );
 };
 
